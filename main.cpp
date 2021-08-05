7b3faef4c9ee93f489e10c5edda45c1ed29d67c7
roberto montalti <roberto.montalti00@gmail.com>
2021-08-05 02:30:13 +0200

    testing aliasing per sample

--- main.cpp
+++ main.cpp
@@ -2,53 +2,57 @@
 #include "ray.h"
 #include "vec3.h"
 #include "sphere.h"
+#include "consts.h"
+#include "cam.h"
 
 #include <iostream>
 
-point3 scenter(3, 0, -2);
-
 //making a linear gradient as background
-color ray_color(const ray& r) {
-    auto t = hit_sphere(scenter, 0.5, r);
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - scenter);
-        return .5*color(N.x() + 1, N.y() + 1, N.z() + 1); //this is for negatives, cant render as color
+color ray_color(const ray& r, const hittable &w) {
+    hit_record rec;
+    if (w.hit(r, 0, inf, rec)) {
+        return 0.5 * (rec.normal + color(1,1,1)); //fix negatives, might fuck up colors
     }
     vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5*(unit_direction.y() + 1.0);
+    auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
 
 int main() {
     // Image
-    const auto aspect_ratio = 16.0 / 9.0;
+    const int samples = 100;
     const int image_width = 400;
+    const auto aspect_ratio = 16.0 / 9.0;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
 
     // Camera
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
+    cam cam;
 
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+    hittable_list w;
+    w.add(make_shared<sphere>(point3(-1,1,-1), 0.5));
+    w.add(make_shared<sphere>(point3(3,0,-1), 0.5));
+    w.add(make_shared<sphere>(point3(8,-3,-10), 2));
+    w.add(make_shared<sphere>(point3(-3,-2,-3), 1));
+    w.add(make_shared<sphere>(point3(6,3,-1), .5));
+    w.add(make_shared<sphere>(point3(4,1,-1), .7));
 
-    auto render = [=](){
+    auto render = [&](){
         std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
         for (int j = image_height-1; j >= 0; --j) {
             std::cerr << "\nScanlines remaining: " << j << ' ' << std::flush;
             for (int i = 0; i < image_width; ++i) {
-                auto u = double(i) / (image_width-1);
-                auto v = double(j) / (image_height-1);
-                //rays are shot from the eye, or the camera -> <0,0,0>
-                ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-                color pixel_color = ray_color(r);
-                write_color(std::cout, pixel_color);
+                color pixel_color(0, 0, 0);
+                for (int s = 0; s < samples; ++s) {
+                    auto u = (i + randouble()) / (image_width-1);
+                    auto v = (j + randouble()) / (image_height-1);
+                    ray r = cam.get_ray(u, v);
+                    pixel_color += ray_color(r, w);
+                }
+                write_color(std::cout, pixel_color, samples);
             }
         }
         std::cerr << "\nDone.\n";
     };
+
     render();
 }
