774acbbe699ae1c4134c84e10077f4e7a64c30a1
roberto montalti <roberto.montalti00@gmail.com>
2021-08-07 14:42:51 +0200

    added ray bouncing till we hit the sky or reach the max recursion depth

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,13 @@
 #include <iostream>
 
 //making a linear gradient as background
-color ray_color(const ray& r, const hittable &w) {
+color ray_color(const ray& r, const hittable &w, int depth) {
     hit_record rec;
+    if (depth <= 0)
+        return color(0,0,0);
     if (w.hit(r, 0, inf, rec)) {
-        return 0.5 * (rec.normal + color(1,1,1)); //fix negatives, might fuck up colors
+        point3 target = rec.p + rec.normal + rand_unit_sphere();
+        return 0.5 * ray_color(ray(rec.p, target - rec.p), w, depth - 1); //fix negatives, might fuck up colors
     }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
@@ -24,17 +27,14 @@ int main() {
     const int image_width = 400;
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int max_depth = 50;
 
     // Camera
     cam cam;
 
     hittable_list w;
-    w.add(make_shared<sphere>(point3(-1,1,-1), 0.5));
-    w.add(make_shared<sphere>(point3(3,0,-1), 0.5));
-    w.add(make_shared<sphere>(point3(8,-3,-10), 2));
-    w.add(make_shared<sphere>(point3(-3,-2,-3), 1));
-    w.add(make_shared<sphere>(point3(6,3,-1), .5));
-    w.add(make_shared<sphere>(point3(4,1,-1), .7));
+    w.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    w.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
     auto render = [&](){
         std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
@@ -46,7 +46,7 @@ int main() {
                     auto u = (i + randouble()) / (image_width-1);
                     auto v = (j + randouble()) / (image_height-1);
                     ray r = cam.get_ray(u, v);
-                    pixel_color += ray_color(r, w);
+                    pixel_color += ray_color(r, w, max_depth);
                 }
                 write_color(std::cout, pixel_color, samples);
             }
